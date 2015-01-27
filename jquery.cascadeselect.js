ab89e7052e29694c4e0c1af946a1533c2aa28c2e
zhangv <derekzhangv@gmail.com>
2015-01-27 15:07:42 +0800

    add us cities

--- jquery.cascadeselect.js
+++ jquery.cascadeselect.js
@@ -19,9 +19,14 @@ $.fn.cascadeselector = function(options){
 			$l2select = $(targets.eq(1));
 			$l1select.on('change',l1Changed);
 			$l2select.on('change',l2Changed);
-			var tmpkeys = keys.split(',');
-			l1key = tmpkeys[0];
-			l2key = tmpkeys[1];
+			if(keys){
+				var tmpkeys = keys.split(',');
+				l1key = tmpkeys[0];
+				l2key = tmpkeys[1];
+			}else{
+
+			}
+
 		} else if (targets.length == 3) {
 			$l1select = $(targets.eq(0));
 			$l2select = $(targets.eq(1));
@@ -89,51 +94,93 @@ $.fn.cascadeselector = function(options){
 
 	function getL1Options() {
 		var l1array = [];
-		for (var i = 0; i < data[l1key].length; i++) {
-			var tmp = data[l1key][i];
-			l1array[i] = {
-				value:tmp[valuekey],
-				label:tmp[labelkey]
-			};
+		if(l1key){
+			for (var i = 0; i < data[l1key].length; i++) {
+				var tmp = data[l1key][i];
+				l1array[i] = {
+					value:tmp[valuekey], label:tmp[labelkey]
+				};
+			}
+		}else{
+			var i = 0;
+			for (prop in data){
+				l1array[i++] = {
+					value:prop, label:prop
+				};
+			}
 		}
+
 		return l1array;
 	}
 
 	function getL2Options(l1value) {
-		for (var i = 0; i < data[l1key].length; i++) {
-			var tmp = data[l1key][i];
-			if (tmp[valuekey] == l1value) {
-				var tmpl2 = tmp[l2key];
+		if(l1key && l2key){
+			for (var i = 0; i < data[l1key].length; i++) {
+				var tmp = data[l1key][i];
+				if (tmp[valuekey] == l1value) {
+					var tmpl2 = tmp[l2key];
+					var l2array = [];
+					for (var j = 0; j < tmpl2.length; j++) {
+						l2array[j] = {
+							value:tmpl2[j][valuekey],
+							label:tmpl2[j][labelkey]
+						};
+					}
+					return l2array;
+				}
+			}
+		}else{
+			for(prop in data){
+				if(l1value != prop) continue;
 				var l2array = [];
-				for (var j = 0; j < tmpl2.length; j++) {
+				for (var j = 0; j < data[prop].length; j++) {
 					l2array[j] = {
-						value:tmpl2[j][valuekey],
-						label:tmpl2[j][labelkey]
+						value:data[prop][j],
+						label:data[prop][j]
 					};
 				}
 				return l2array;
 			}
 		}
+
 		return [];
 	}
 
 	function getL3Options(l1value,l2value) {
-		for (var i = 0; i < data[l1key].length; i++) {
-			var l1 = data[l1key][i];
-			if(l1[valuekey] != l1value) continue;
-			for (var j = 0; j < l1[l2key].length; j++) {
-				var l2 = l1[l2key][j];
-				if (l2[valuekey] == l2value) {
-					var l3s = l2[l3key];
+		if(l1key && l2key && l3key) {
+			for (var i = 0; i < data[l1key].length; i++) {
+				var l1 = data[l1key][i];
+				if (l1[valuekey] != l1value) continue;
+				for (var j = 0; j < l1[l2key].length; j++) {
+					var l2 = l1[l2key][j];
+					if (l2[valuekey] == l2value) {
+						var l3s = l2[l3key];
+						var l3array = [];
+						for (var k = 0; k < l3s.length; k++) {
+							l3array[k] = {
+								value: l3s[k][valuekey],
+								label: l3s[k][labelkey]
+							};
+						}
+						return l3array;
+					}
+				}
+			}
+		}else{
+			for(prop in data){
+				if(l1value != prop) continue;
+				for(prop2 in data[prop]){
+					if(l2value != prop2) continue;
 					var l3array = [];
-					for (var k = 0; k < l3s.length; k++) {
-						l3array[k] = {
-							value:l3s[k][valuekey],
-							label:l3s[k][labelkey]
+					for (var j = 0; j < data[prop][prop2].length; j++) {
+						l3array[j] = {
+							value:data[prop][prop2][j],
+							label:data[prop][prop2][j]
 						};
 					}
 					return l3array;
 				}
+
 			}
 		}
 	}
